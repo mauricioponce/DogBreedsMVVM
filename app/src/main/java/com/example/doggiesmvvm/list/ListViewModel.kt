1b6049dfb97125770d6501202d0080f758299b96
Mauricio Ponce Oyarzún <mauricio.ponce.o@gmail.com>
2020-08-29 14:36:18 -0400

    [CHG] shared viewmodel para mostrar la raza seleccionada en el detalle

--- app/src/main/java/com/example/doggiesmvvm/list/ListViewModel.kt
+++ app/src/main/java/com/example/doggiesmvvm/list/ListViewModel.kt
@@ -1,16 +1,15 @@
 package com.example.doggiesmvvm.list
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Transformations
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.example.doggiesmvvm.model.Repository
 import com.example.doggiesmvvm.model.db.BreedEntity
 
 class ListViewModel(application: Application) : AndroidViewModel(application) {
 
     private val repository = Repository(application, viewModelScope)
+
+    var selected = MutableLiveData<DoggyUI>()
 
     val doggiesList: LiveData<List<DoggyUI>> = Transformations.map(repository.getDoggies()) {
         it.map { breed -> convert(breed) }
@@ -21,5 +20,9 @@ class ListViewModel(application: Application) : AndroidViewModel(application) {
      */
     private fun convert(d: BreedEntity) : DoggyUI {
         return DoggyUI(d.breedName)
+    }
+
+    fun select(it: DoggyUI) {
+        selected.postValue(it)
     }
 }
